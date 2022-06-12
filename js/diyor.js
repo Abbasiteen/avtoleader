6b235ab1b6e9cc2a588493c4e57d90f97688cb21
diy0r4ik <95129772+diy0r4ik@users.noreply.github.com>
2022-06-12 15:56:07 +0500

    sfd
    sfbs

--- js/diyor.js
+++ js/diyor.js
@@ -1,26 +1,26 @@
-let wrapper = document.querySelector(".wrapper");
+// let wrapper = document.querySelector(".wrapper");
 
 
-// fetch("https://jsonplaceholder.typicode.com/users")
+// // fetch("https://jsonplaceholder.typicode.com/users")
+// // .then((res) => res.json())
+// // .then((users) =>  console.log(users))
+
+
+// fetch("#A")
 // .then((res) => res.json())
-// .then((users) =>  console.log(users))
+// .then((A) => console.log(A),
 
-
-fetch("#A")
-.then((res) => res.json())
-.then((A) => console.log(A),
-
-        A.Search.forEach(element => {
-            console.log(element);
-            wrapper.innerHTML +=`
-            <div class="card">
-            <div class="img">
-            <p>${element.name}</p>
-            </div>
-                <h2>${element.Title}</h2>
-                <p>${element.Year}</p>
-            </div>
-            `
-            ;
-        })
-);
+//         A.Search.forEach(element => {
+//             console.log(element);
+//             wrapper.innerHTML +=`
+//             <div class="card">
+//             <div class="img">
+//             <p>${element.name}</p>
+//             </div>
+//                 <h2>${element.Title}</h2>
+//                 <p>${element.Year}</p>
+//             </div>
+//             `
+//             ;
+//         })
+// );
--- js/json.js
+++ js/json.js
@@ -1,154 +1,154 @@
-var A = [
-    {
-        id:1,
-        name:'Range Rover S64 Coupe',
-        rate: 5,
-        sena:'$250/',
-        door:4,
-        seat:5,
-        transmision:'automatic',
-        age:18
-    },
-    {
-        id:2,
-        name:'Range Rover S64 Coupe',
-        rate: 5,
-        sena:'$250/',
-        door:4,
-        seat:5,
-        transmision:'automatic',
-        age:18
-    },
-    {
-        id:3,
-        name:'Range Rover S64 Coupe',
-        rate: 5,
-        sena:'$250/',
-        door:4,
-        seat:5,
-        transmision:'automatic',
-        age:18
-    },
-    {
-        id:4,
-        name:'Range Rover S64 Coupe',
-        rate: 5,
-        sena:'$250/',
-        door:4,
-        seat:5,
-        transmision:'automatic',
-        age:18
-    },
-    {
-        id:5,
-        name:'Range Rover S64 Coupe',
-        rate: 5,
-        sena:'$250/',
-        door:4,
-        seat:5,
-        transmision:'automatic',
-        age:18
-    },
-    {
-        id:6,
-        name:'Range Rover S64 Coupe',
-        rate: 5,
-        sena:'$250/',
-        door:4,
-        seat:5,
-        transmision:'automatic',
-        age:18
-    },
-    {
-        id:7,
-        name:'Range Rover S64 Coupe',
-        rate: 5,
-        sena:'$250/',
-        door:4,
-        seat:5,
-        transmision:'automatic',
-        age:18
-    },
-    {
-        id:8,
-        name:'Range Rover S64 Coupe',
-        rate: 5,
-        sena:'$250/',
-        door:4,
-        seat:5,
-        transmision:'automatic',
-        age:18
-    },
-    {
-        id:9,
-        name:'Range Rover S64 Coupe',
-        rate: 5,
-        sena:'$250/',
-        door:4,
-        seat:5,
-        transmision:'automatic',
-        age:18
-    },
-    {
-        id:10,
-        name:'Range Rover S64 Coupe',
-        rate: 5,
-        sena:'$250/',
-        door:4,
-        seat:5,
-        transmision:'automatic',
-        age:18
-    },
-    {
-        id:11,
-        name:'Range Rover S64 Coupe',
-        rate: 5,
-        sena:'$250/',
-        door:4,
-        seat:5,
-        transmision:'automatic',
-        age:18
-    },
-    {
-        id:12,
-        name:'Range Rover S64 Coupe',
-        rate: 5,
-        sena:'$250/',
-        door:4,
-        seat:5,
-        transmision:'automatic',
-        age:18
-    },
-    {
-        id:13,
-        name:'Range Rover S64 Coupe',
-        rate: 5,
-        sena:'$250/',
-        door:4,
-        seat:5,
-        transmision:'automatic',
-        age:18
-    },
-    {
-        id:19,
-        name:'Range Rover S64 Coupe',
-        rate: 5,
-        sena:'$250/',
-        door:4,
-        seat:5,
-        transmision:'automatic',
-        age:18
-    },
-    {
-        id:20,
-        name:'Range Rover S64 Coupe',
-        rate: 5,
-        sena:'$250/',
-        door:4,
-        seat:5,
-        transmision:'automatic',
-        age:18
-    },
+// var A = [
+//     {
+//         id:1,
+//         name:'Range Rover S64 Coupe',
+//         rate: 5,
+//         sena:'$250/',
+//         door:4,
+//         seat:5,
+//         transmision:'automatic',
+//         age:18
+//     },
+//     {
+//         id:2,
+//         name:'Range Rover S64 Coupe',
+//         rate: 5,
+//         sena:'$250/',
+//         door:4,
+//         seat:5,
+//         transmision:'automatic',
+//         age:18
+//     },
+//     {
+//         id:3,
+//         name:'Range Rover S64 Coupe',
+//         rate: 5,
+//         sena:'$250/',
+//         door:4,
+//         seat:5,
+//         transmision:'automatic',
+//         age:18
+//     },
+//     {
+//         id:4,
+//         name:'Range Rover S64 Coupe',
+//         rate: 5,
+//         sena:'$250/',
+//         door:4,
+//         seat:5,
+//         transmision:'automatic',
+//         age:18
+//     },
+//     {
+//         id:5,
+//         name:'Range Rover S64 Coupe',
+//         rate: 5,
+//         sena:'$250/',
+//         door:4,
+//         seat:5,
+//         transmision:'automatic',
+//         age:18
+//     },
+//     {
+//         id:6,
+//         name:'Range Rover S64 Coupe',
+//         rate: 5,
+//         sena:'$250/',
+//         door:4,
+//         seat:5,
+//         transmision:'automatic',
+//         age:18
+//     },
+//     {
+//         id:7,
+//         name:'Range Rover S64 Coupe',
+//         rate: 5,
+//         sena:'$250/',
+//         door:4,
+//         seat:5,
+//         transmision:'automatic',
+//         age:18
+//     },
+//     {
+//         id:8,
+//         name:'Range Rover S64 Coupe',
+//         rate: 5,
+//         sena:'$250/',
+//         door:4,
+//         seat:5,
+//         transmision:'automatic',
+//         age:18
+//     },
+//     {
+//         id:9,
+//         name:'Range Rover S64 Coupe',
+//         rate: 5,
+//         sena:'$250/',
+//         door:4,
+//         seat:5,
+//         transmision:'automatic',
+//         age:18
+//     },
+//     {
+//         id:10,
+//         name:'Range Rover S64 Coupe',
+//         rate: 5,
+//         sena:'$250/',
+//         door:4,
+//         seat:5,
+//         transmision:'automatic',
+//         age:18
+//     },
+//     {
+//         id:11,
+//         name:'Range Rover S64 Coupe',
+//         rate: 5,
+//         sena:'$250/',
+//         door:4,
+//         seat:5,
+//         transmision:'automatic',
+//         age:18
+//     },
+//     {
+//         id:12,
+//         name:'Range Rover S64 Coupe',
+//         rate: 5,
+//         sena:'$250/',
+//         door:4,
+//         seat:5,
+//         transmision:'automatic',
+//         age:18
+//     },
+//     {
+//         id:13,
+//         name:'Range Rover S64 Coupe',
+//         rate: 5,
+//         sena:'$250/',
+//         door:4,
+//         seat:5,
+//         transmision:'automatic',
+//         age:18
+//     },
+//     {
+//         id:19,
+//         name:'Range Rover S64 Coupe',
+//         rate: 5,
+//         sena:'$250/',
+//         door:4,
+//         seat:5,
+//         transmision:'automatic',
+//         age:18
+//     },
+//     {
+//         id:20,
+//         name:'Range Rover S64 Coupe',
+//         rate: 5,
+//         sena:'$250/',
+//         door:4,
+//         seat:5,
+//         transmision:'automatic',
+//         age:18
+//     },
     // {
     //     id:3,
     //     name:'Range Rover S64 Coupe',
@@ -259,4 +259,4 @@ var A = [
     //     transmision:'automatic',
     //     age:18
     // },
-]
+// ]
